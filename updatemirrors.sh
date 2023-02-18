a046465882a5c48bc310bfc612cb65188ce80a0a
tylort69 <porsht@hotmail.com>
2023-02-18 03:06:15 -0500

    added the shell script

---
+++ updatemirrors.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dateandtime=$(date +%F-%H-%M-%S)
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist"${dateandtime}".bak
+echo "Mirror file backup created: /etc/pacman.d/mirrorlist${dateandtime}.bak"
+sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo "Mirror list update complete."
